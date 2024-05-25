f650ec78ab414504973f145af9d5ac4c02ca0699
JoseTarinT <joseluis_tt88@hotmail.com>
2024-05-25 21:48:11 +1000

    Delete unneed files

--- src/components/contactButton.jsx
+++
@@ -1,8 +0,0 @@
-const Contact = () => {
-  const handleClick = () => {
-    console.log("My contact number is: ...");
-  };
-  return <button onClick={handleClick}>Contact</button>;
-};
-
-export default Contact;
